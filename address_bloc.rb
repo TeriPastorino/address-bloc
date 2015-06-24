dfc0dc45a48c14d978a3c4412643e0ae865cd84f
Teri Pastorino <tpasto@comcast.net>
2015-06-24 13:39:03 -0700

    add remove_entry spec

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,34 @@
 puts "Welcome to AddressBloc!"
+
+
+
+def greeting
+
+    ARGV.each do |name|
+        puts " #{name}!"
+    end
+end
+
+greeting
+
+def cookies
+    ARGV.each do |cookie|
+        puts "my favorite cookie is #{cookie}"
+    end
+end
+
+cookies
+
+def hello_world
+    # #1
+    ARGV.each do |arg|
+        puts "Hello, #{arg}!"
+    end
+end
+
+hello_world
+
+at prompt
+$ ruby hello_world.rb Kermit Piggy Statler Waldorf Janice Fozzie Camilla
+
+
