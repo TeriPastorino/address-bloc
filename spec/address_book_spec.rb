6348c3c9f08c7f8c5e7b19bd120d6c7c0406f950
Teri Pastorino <tpasto@comcast.net>
2015-06-24 13:40:05 -0700

    add remove_entry_spec

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,13 @@ RSpec.describe AddressBook do
      expect(new_entry.email).to eq 'augusta.king@lovelace.com'
    end
   end
+
+  context "#remove_entry" do
+    it "removes one entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      entry = book.entries.last      
+      expect{book.remove_entry(entry)}.to change{book.entries.count}.by(-1)
+    end
+  end
 end
